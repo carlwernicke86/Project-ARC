3ad8763aa16dc20e9d11e82ff89f18813cfaaca4
andyclee <andylee4632@gmail.com>
2016-04-13 14:43:02 -0700

    Update arc_missionList.py

--- arc_missionList.py
+++ arc_missionList.py
@@ -1,5 +1,5 @@
 mission_list = [
-    ("Bob Ross", "U.S Department of Art", "Easy", ("Happy Little Accidents", ""), "Signed Painting", None), #Mission 1
+    ("Dan Li", "Saint Mario Secondary Academy", "Easy", ("None", ""), "$500", None), #Mission 1
     ("Osama been Laggen", "Minecraft Servers", "Hard", ("Minecraft Account", ""), "Diamond Sword", None),   #Mission 2
     ("Daniel Lee", "SMHS Room 305", "I don't know man, let me look at your code", ("None apparently", ""), "I know where you guys are at", None), #Mission 3
     ("Big Boss", "grey metropolis", "Medium", ("None", ""), "$$$", None)
