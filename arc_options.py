2879e8003618a1732184f1e8c96acbd682386e05
gymnast86 <stock.joann@gmail.com>
2016-04-22 20:06:20 -0700

    Update arc_options.py

--- arc_options.py
+++ arc_options.py
@@ -2,7 +2,7 @@ from other_objects import *
 import pygame, sys
 
 screen = pygame.display.set_mode((WIN_W, WIN_H), pygame.SRCALPHA)
-def options(screen):
+def options():
     ControlOptions = open('ControlOptions.txt', 'r')
     jump_button = ControlOptions.readline()
     left_button = ControlOptions.readline()
@@ -13,7 +13,7 @@ def options(screen):
     jump_button = jump_button[0:len(jump_button) - 1]
     left_button = left_button[0:len(left_button) - 1]
     right_button = right_button[0:len(right_button) - 1]
-    interact_button = interact_button[0]
+    interact_button = interact_button
 
 
     for k in key_list:
@@ -41,14 +41,59 @@ def options(screen):
     click_button_group = pygame.sprite.Group()
     click_button_group.add(exit)
 
+    jump_button = jump.button
+    left_button = left.button
+    right_button = right.button
+    interact_button = interact.button
+    
     option = True
     while option:
         mouse_pos = pygame.mouse.get_pos()
         for event in pygame.event.get():
+            old_jump = jump.button
             jump_button = jump.update(event, option_text_group,mouse_pos)
+            old_left = left.button
             left_button = left.update(event, option_text_group,mouse_pos)
+            old_right = right.button
             right_button = right.update(event, option_text_group,mouse_pos)
+            old_interact = interact.button
             interact_button = interact.update(event, option_text_group,mouse_pos)
+
+
+            if jump.button == left.button:
+                if left.selected == True:
+                    jump.button = old_left
+                elif jump.selected == True:
+                    left.button = old_jump
+            elif jump.button == right.button:
+                if jump.selected == True:
+                    right.button = old_jump
+                elif right.selected == True:
+                    jump.button = old_right
+            elif jump.button == interact.button:
+                if jump.selected == True:
+                    interact.button = old_jump
+                elif interact.selected == True:
+                    jump.button = old_interact
+
+            if left.button == right.button:
+                if left.selected == True:
+                    right.button = old_left
+                elif right.selected == True:
+                    left.button = old_right
+            elif left.button == interact.button:
+                if left.selected == True:
+                    interact.button = old_left
+                elif interact.selected == True:
+                    left.button = old_interact
+
+            if right.button == interact.button:
+                if right.selected == True:
+                    interact.button = old_right
+                elif interact.selected == True:
+                    right.button = old_interact
+
+
             ControlOptions = open('ControlOptions.txt', 'w')
             ControlOptions.write(jump_button+"\n")
             ControlOptions.write(left_button+"\n")
