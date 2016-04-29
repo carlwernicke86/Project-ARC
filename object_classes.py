bd487e10a0e7baf0bdf75691d844f0121edad886
LegendRK <card.game.champion@gmail.com>
2016-04-29 14:09:29 -0700

    Update object_classes.py

--- object_classes.py
+++ object_classes.py
@@ -613,18 +613,22 @@ class ElevatorDoor(pygame.sprite.Sprite):
 #I HAVE NO IDEA WHY THE ELEVATOR WORKS, I JUST KNOW THAT IT DOES
 #END ELEVATOR CODE
 
-class Event(pygame.sprite.Sprite):
+class Event_Mission03(pygame.sprite.Sprite):
     def __init__(self, x, y, x_range, y_range, id):
         pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
-        self.image = pygame.Surface((x_range*self.x, y_range*self.y)).convert()
-        self.rect = self.image.get_rect
+        self.image = pygame.Surface((32*x_range, 32*y_range)).convert()
+        self.rect = self.image.get_rect()
+        self.rect.x = self.x
+        self.rect.y = self.y - 96
         self.id = id
 
     def update(self, hero, event_list):
 
-         collision = pygame.sprite.collide_rect(self, hero)
+        collision = pygame.sprite.collide_rect(self, hero)
 
-         if collision == True:
+        if collision == True:
              event_list[self.id-1] = 1
+
+        return event_list
