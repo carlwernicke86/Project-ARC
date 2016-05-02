3c5f5842d5602ded665cdb3c296de2e18d86465e
gymnast86 <stock.joann@gmail.com>
2016-05-02 14:48:48 -0700

    Added files via upload

--- arc_main.py
+++ arc_main.py
@@ -1,6 +1,7 @@
 import pygame, os
 from arc_intro import intro
 from HubLocation import hub
+from other_objects import *
 
 
 fps = 60
@@ -18,10 +19,24 @@ screen = pygame.display.set_mode((WIN_W, WIN_H), pygame.SRCALPHA)              #
 clock = pygame.time.Clock()            #The clock which can be used to set fps
 beg_time = pygame.time.get_ticks()     #The time the game first begins
 
+
 def main():
+    ControlOptions = open('ControlOptions.txt', 'r')
+    if ControlOptions.readline() == "W\n" and ControlOptions.readline() == "A\n" and ControlOptions.readline() == "D\n" and ControlOptions.readline() == "E":
+        ControlOptions.close()
+        ControlOptions = open('ControlOptions.txt', "w")
+        ControlOptions.write("W"+"\n")
+        ControlOptions.write("A"+"\n")
+        ControlOptions.write("D"+"\n")
+        ControlOptions.write("E")
+    ControlOptions.close()
+
+
     TIMER = 0
-    intro(screen, clock, fps, TIMER)
-    hub(screen, clock, fps, TIMER)
+    structure_loop = True
+    while structure_loop:
+        intro(screen, clock, fps, TIMER)
+        structure_loop = hub(screen, clock, fps, TIMER)
 
 
 
