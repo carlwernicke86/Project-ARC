33f4806eb2974af4d06e997a7dffe713e11b19df
gymnast86 <stock.joann@gmail.com>
2016-05-06 14:37:39 -0700

    Update arc_missionList.py

--- arc_missionList.py
+++ arc_missionList.py
@@ -1,8 +1,10 @@
 from ARC_Mission01 import mission01
+from ARC_Mission02 import mission02
+from ARC_Mission03 import mission03
 
 mission_list = [
     ("Dan Li", "Saint Mario Secondary Academy", "Easy", ("None", ""), "$500", mission01), #Mission 1
-    ("Osama been Laggen", "Minecraft Servers", "Hard", ("Minecraft Account", ""), "Diamond Sword", None),   #Mission 2
-    ("Daniel Lee", "SMHS Room 305", "I don't know man, let me look at your code", ("None apparently", ""), "I know where you guys are at", None), #Mission 3
+    ("Osama been Laggen", "Minecraft Servers", "Hard", ("Minecraft Account", ""), "Diamond Sword", mission02),   #Mission 2
+    ("Daniel Lee", "SMHS Room 305", "I don't know man, let me look at your code", ("None apparently", ""), "I know where you guys are at", mission03), #Mission 3
     ("Big Boss", "grey metropolis", "Medium", ("None", ""), "$$$", None)
 ]
