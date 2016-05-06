23a505fdce38225c41c48e356fdc13e8045f198d
andyclee <andylee4632@gmail.com>
2016-05-06 14:44:10 -0700

    Update arc_missionList.py

--- arc_missionList.py
+++ arc_missionList.py
@@ -4,7 +4,7 @@ from ARC_Mission03 import mission03
 
 mission_list = [
     ("Dan Li", "Saint Mario Secondary Academy", "Easy", ("None", ""), "$500", mission01), #Mission 1
-    ("Osama been Laggen", "Minecraft Servers", "Hard", ("Minecraft Account", ""), "Diamond Sword", mission02),   #Mission 2
-    ("Daniel Lee", "SMHS Room 305", "I don't know man, let me look at your code", ("None apparently", ""), "I know where you guys are at", mission03), #Mission 3
+    ("ARC Corp", "DataTech Solutions", "Medium", ("None", ""), "$7000", mission02),   #Mission 2
+    ("ARC Corp", "Juno Defense Group", "Medium", ("None", ""), "$15000", mission03), #Mission 3
     ("Big Boss", "grey metropolis", "Medium", ("None", ""), "$$$", None)
 ]
