9dd90ad8bf383253b97c13cdd38cd1c5bdac7c3d
andyclee <andylee4632@gmail.com>
2016-05-11 14:47:07 -0700

    Update ARC_Mission03.py

--- ARC_Mission03.py
+++ ARC_Mission03.py
@@ -27,7 +27,7 @@ def mission03(intro_flag = False):
     screen = pygame.display.set_mode((WIN_W, WIN_H), pygame.SRCALPHA)
 
     level3 = Regular_Text(100, BLACK, (screen.get_rect().centerx, screen.get_rect().centery/2), "Level 3")
-    press_continue = Regular_Text(50, (200, 200, 200), (screen.get_rect().centerx, screen.get_rect().centery), "- Press any button to Proceed -")
+    press_continue = Regular_Text(50, (200, 200, 200), (screen.get_rect().centerx, screen.get_rect().centery), "- Press Any Button to Proceed -")
     
     #Group creation
     platform_group = pygame.sprite.Group()
