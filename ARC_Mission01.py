6267cda2f5cabad71b4293cd8d1a12f66f1b5bb5
andyclee <andylee4632@gmail.com>
2016-05-18 14:17:08 -0700

    Update ARC_Mission01.py

--- ARC_Mission01.py
+++ ARC_Mission01.py
@@ -14,6 +14,13 @@ WIN_H = 900
 
 def mission01(intro_flag = False):
     pygame.init()
+    
+    try:
+        pygame.mixer.music.stop()
+        pygame.mixer.music.load("Sounds/TakingDarkSong.mp3")
+        pygame.mixer.music.play(-1)
+    except pygame.error:
+        pass
 
     #Basic settings
     mission01_loop = True
