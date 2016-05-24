48a2cc7f26bb28b24f2c4a6c17c7a32ff0a1efd7
andyclee <andylee4632@gmail.com>
2016-05-24 13:51:47 -0700

    Update object_classes.py

--- object_classes.py
+++ object_classes.py
@@ -520,6 +520,12 @@ class GenericTrigger(pygame.sprite.Sprite):
     def update(self, hero):
         if pygame.sprite.collide_rect(self, hero):
             self.active = True #[TRIGGERED]
+            
+class BackgroundImage(pygame.sprite.Sprite):
+    def __init__(self, image_path):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.rect = self.image.get_rect()
 
 #ELEVATOR OBJECTS
 class ElevatorFloor(pygame.sprite.Sprite):
