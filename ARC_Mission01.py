e4eb36be0d9de0a96c616e6cebdf0a2e021c2ed5
gymnast86 <stock.joann@gmail.com>
2016-05-24 14:11:46 -0700

    Update ARC_Mission01.py

--- ARC_Mission01.py
+++ ARC_Mission01.py
@@ -145,9 +145,9 @@ def mission01(intro_flag = False):
                 sys.exit()
 
         # Update
+        hero_group.update(platform_group, mission01)
+        camera.update(hero.rect)
         if hero.activate_caught == False:
-            hero_group.update(platform_group, mission01)
-            camera.update(hero.rect)
             secguard_group.update(hero, secguard_group, mission01)
             trig1.update(hero)
             triggerdoor1.update(trig1)
