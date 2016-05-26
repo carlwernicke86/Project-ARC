5becbc5da87eeb20ded36b1aa7aaf794106a79ca
gymnast86 <stock.joann@gmail.com>
2016-05-26 14:46:59 -0700

    Update arc_intro.py

--- arc_intro.py
+++ arc_intro.py
@@ -1,6 +1,7 @@
 import pygame, sys
 from other_objects import *
 from arc_options import options
+from arc_credits import credits
 
 def intro(screen, clock, fps, TIMER):
     TIMER += 1
@@ -9,16 +10,16 @@ def intro(screen, clock, fps, TIMER):
     Title = Regular_Text(80, ORANGE, (screen.get_rect().centerx, screen.get_rect().centery/2), "Project ARC")
 
     #Click Text Objects
-    Continue = Click_Button(40, ORANGE, LIGHT_GREY, (screen.get_rect().centerx, screen.get_rect().centery/1.5), "Continue", False, None, None, None, WHITE)
-    New_Game = Click_Button(40, ORANGE, LIGHT_GREY, (screen.get_rect().centerx, Continue.rect.y + 65), "Play", False, None, None, None, WHITE)
+    New_Game = Click_Button(40, ORANGE, LIGHT_GREY, (screen.get_rect().centerx, screen.get_rect().centery/1.5), "Play", False, None, None, None, WHITE)
     Options = Click_Button(40, ORANGE, LIGHT_GREY, (screen.get_rect().centerx, New_Game.rect.y + 65), "Options", options, None, None, None, WHITE)
+    Credits = Click_Button(40, ORANGE, LIGHT_GREY, (screen.get_rect().centerx, Options.rect.y + 65), "Credits", credits, None, None, None, WHITE)
     
     #Groups and adding things to groups
     regular_text_group = pygame.sprite.Group()
     regular_text_group.add(Title)
 
     click_button_group = pygame.sprite.Group()
-    click_button_group.add(New_Game, Options)
+    click_button_group.add(New_Game, Options, Credits)
 
     bg = pygame.image.load("Sprites/MainBack.png")
 
