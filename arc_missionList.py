282bdc2d3940c6a54ee8fc1fb402a5f736e04e30
gymnast86 <stock.joann@gmail.com>
2016-05-26 14:50:49 -0700

    Update arc_missionList.py

--- arc_missionList.py
+++ arc_missionList.py
@@ -1,11 +1,13 @@
 from ARC_Mission01 import mission01
 from ARC_Mission02 import mission02
 from ARC_Mission03 import mission03
+from ARC_Mission04 import mission04
+from ARC_Mission05 import mission05
 
 mission_list = [
     ("Dan Li", "Saint Mario Secondary Academy", "Easy", ("None", ""), "$500", mission01), #Mission 1
     ("ARC Corp", "DataTech Solutions", "Medium", ("None", ""), "$7000", mission02),   #Mission 2
     ("ARC Corp", "Juno Defense Group", "Medium", ("None", ""), "$15000", mission03), #Mission 3
-    ("Intelligent Secure Solutions", "Maclaurin Group Warehouse #14", "Hard", ("None", ""), "$20000", None) #Mission 4
-    ("Aegis Data Security", "Athena Electronics", "Hard", ("None", ""), "$20000", None) #Mission 5
+    ("Intelligent Secure Solutions", "Maclaurin Group Warehouse #14", "Hard", ("None", ""), "$20000", mission04), #Mission 4
+    ("Aegis Data Security", "Athena Electronics", "Hard", ("None", ""), "$20000", mission05) #Mission 5
 ]
